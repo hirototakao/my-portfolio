bd421d0d9c27e40e849d3b69d06bdf85e681aab8
hiroto takao <biiiy080121@outlook.com>
2024-02-07 13:43:04 +0900

    fix design

--- contact/contact.js
+++ contact/contact.js
@@ -1,14 +1,20 @@
 function clipboard(id) {
-  $(function() {
     const emailSpan = document.getElementById(id);
     const email = emailSpan.innerText;
     navigator.clipboard.writeText(email);
-  });
 }
 function clipboardForTextBox(id) {
-  $(function() {
     const Input = document.getElementById(id);
     const InputValue = Input.getAttribute('value');
     navigator.clipboard.writeText(InputValue);
-  });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const twitterIcon = document.getElementById('twitterIcon');
+  const githubIcon = document.getElementById('githubIcon');
+  twitterIcon.setAttribute('height', '25');
+  twitterIcon.setAttribute('width', '30');
+
+  githubIcon.setAttribute('height', '27');
+  githubIcon.setAttribute('width', '30');
+});
