bf575fd69c5e0ee8e20db58c64ae4eda1b3060fc
hiroto takao <biiiy080121@outlook.com>
2024-04-30 19:31:42 +0900

    fixing design 2

--- about/my-portfolio.js
+++ about/my-portfolio.js
@@ -46,9 +46,9 @@ function closePopUp() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  const flex = document.getElementById("flex");    
   console.log(document.documentElement.clientWidth);
-  if(document.documentElement.clientWidth <= 769){
-    const flex = document.getElementById("flex");    
+  if(document.documentElement.clientWidth >= 465 && document.documentElement.clientWidth <= 769){
     flex.innerHTML = `
       <div class="flex" id="flex">
         <div style="width:45%; height:400px;">
@@ -63,8 +63,8 @@ window.addEventListener("DOMContentLoaded", () => {
         </div>
         <div style="position:relative; width:10%; text-align:center; margin:0 auto;">
           <div class="timeframe-line"></div>
-         <div class="event-point" style="margin:50px auto;"></div>
-         <div class="event-point" style="margin:400px auto;"></div>
+         <div class="event-point" style="height:50px; margin:50px auto;"></div>
+         <div class="event-point" style="height:50px; margin:400px auto;"></div>
         </div>
       <div style= "width:45%; height:400px;">
           <div style="margin-top:365px; width:100%;">
@@ -76,7 +76,35 @@ window.addEventListener("DOMContentLoaded", () => {
           </div>
       </div>
       </div>`;
-  }
+   } if(document.documentElement.clientWidth < 465){
+    flex.innerHTML = `
+      <div class="flex" id="flex">
+        <div style="width:45%; height:400px;">
+          <div style="margin-top:10px; width:100%;">
+              <div class="text-center display-4">April, 2023</div>
+              <div style="border:2px solid red; width:100%;"></div>
+              <div class="text-center" style="margin:32px 0 22px 0;">
+                <span class="h5">Entered <a href="https://nnn.ed.jp/">N high school</a></span>
+              </div>
+              <p style="width:100%;">N high school, which changed my life in any way such as programming, clubs, N-yobikou and events,  which the school able to provides. It's always keeping me busy. I can say that my life-changing started from here.</p>
+          </div>
+        </div>
+        <div style="position:relative; width:10%; text-align:center; margin:0 auto;">
+          <div class="timeframe-line"></div>
+         <div class="event-point" style="height:35px; margin:50px auto;"></div>
+         <div class="event-point" style="height:35px; margin:400px auto;"></div>
+        </div>
+      <div style= "width:45%; height:400px;">
+          <div style="margin-top:365px; width:100%;">
+              <div class="text-center display-4">April, 2024</div>
+              <div class="line"></div>
+              <div class="text-center" style="margin:32px 0 32px 0;">
+                <span class="h5" id="webpage-width"></span>
+            </div>
+          </div>
+      </div>
+      </div>`;
+   }
   window.setTimeout(function() {
     loadingPage.style.display = "none";   
     document.body.style.backgroundColor = "white";
