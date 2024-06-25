ccd1b1f155af41e23860b8b83bd23fc3f014abae
hiroto takao <biiiy080121@outlook.com>
2024-06-26 08:34:56 +0900

    Fix bug in my-portfolio.js, that loading motion didn't work.

--- about/my-portfolio.js
+++ about/my-portfolio.js
@@ -46,70 +46,75 @@ function closePopUp() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  const flex = document.getElementById("flex");    
-  console.log(document.documentElement.clientWidth);
-  if(document.documentElement.clientWidth >= 465 && document.documentElement.clientWidth <= 769){
-    flex.innerHTML = `
-      <div class="flex" id="flex">
-        <div style="width:45%; height:400px;">
-          <div style="margin-top:10px; width:100%;">
-              <div class="text-center display-4">April, 2023</div>
-              <div style="border:2px solid red; width:100%;"></div>
-              <div class="text-center" style="margin:32px 0 22px 0;">
-                <span class="h5">Entered <a href="https://nnn.ed.jp/">N high school</a></span>
-              </div>
-              <p style="width:100%;">N high school, which changed my life in any way such as programming, clubs, N-yobikou and events,  which the school able to provides. It's always keeping me busy. I can say that my life-changing started from here.</p>
-          </div>
-        </div>
-        <div style="position:relative; width:10%; text-align:center; margin:0 auto;">
-          <div class="timeframe-line"></div>
-         <div class="event-point" style="height:50px; margin:50px auto;"></div>
-         <div class="event-point" style="height:50px; margin:400px auto;"></div>
-        </div>
-      <div style= "width:45%; height:400px;">
-          <div style="margin-top:365px; width:100%;">
-              <div class="text-center display-4">April, 2024</div>
-              <div class="line"></div>
-              <div class="text-center" style="margin:32px 0 32px 0;">
-                <span class="h5" id="webpage-width"></span>
-            </div>
-          </div>
-      </div>
-      </div>`;
-   } if(document.documentElement.clientWidth < 465){
-    flex.innerHTML = `
-      <div class="flex" id="flex">
-        <div style="width:45%; height:400px;">
-          <div style="margin-top:10px; width:100%;">
-              <div class="text-center display-4">April, 2023</div>
-              <div style="border:2px solid red; width:100%;"></div>
-              <div class="text-center" style="margin:32px 0 22px 0;">
-                <span class="h5">Entered <a href="https://nnn.ed.jp/">N high school</a></span>
-              </div>
-              <p style="width:100%;">N high school, which changed my life in any way such as programming, clubs, N-yobikou and events,  which the school able to provides. It's always keeping me busy. I can say that my life-changing started from here.</p>
-          </div>
-        </div>
-        <div style="position:relative; width:10%; text-align:center; margin:0 auto;">
-          <div class="timeframe-line"></div>
-         <div class="event-point" style="height:35px; margin:50px auto;"></div>
-         <div class="event-point" style="height:35px; margin:400px auto;"></div>
-        </div>
-      <div style= "width:45%; height:400px;">
-          <div style="margin-top:365px; width:100%;">
-              <div class="text-center display-4">April, 2024</div>
-              <div class="line"></div>
-              <div class="text-center" style="margin:32px 0 32px 0;">
-                <span class="h5" id="webpage-width"></span>
-            </div>
-          </div>
-      </div>
-      </div>`;
-   }
+  const loadingPage = document.getElementById("loading-page");  
+  loadingPage.style.display = "block";
+  document.body.style.backgroundColor = "yellow";
+  document.getElementById("main-page").style.display = "none";
   setTimeout(function() {
     loadingPage.style.display = "none";   
     document.body.style.backgroundColor = "white";
+    console.log("body's background became white.");
     document.getElementById("main-page").style.display = "block";
-    new TypeIt("#navbar-brand", {
+    const flex = document.getElementById("flex");    
+    console.log(document.documentElement.clientWidth);
+    if(document.documentElement.clientWidth >= 465 && document.documentElement.clientWidth <= 769){
+      flex.innerHTML = `
+        <div class="flex" id="flex">
+          <div style="width:45%; height:400px;">
+            <div style="margin-top:10px; width:100%;">
+                <div class="text-center display-4">April, 2023</div>
+                <div style="border:2px solid red; width:100%;"></div>
+                <div class="text-center" style="margin:32px 0 22px 0;">
+                  <span class="h5">Entered <a href="https://nnn.ed.jp/">N high school</a></span>
+                </div>
+                <p style="width:100%;">N high school, which changed my life in any way such as programming, clubs, N-yobikou and events,  which the school able to provides. It's always keeping me busy. I can say that my life-changing started from here.</p>
+            </div>
+          </div>
+          <div style="position:relative; width:10%; text-align:center; margin:0 auto;">
+            <div class="timeframe-line"></div>
+           <div class="event-point" style="height:50px; margin:50px auto;"></div>
+           <div class="event-point" style="height:50px; margin:400px auto;"></div>
+          </div>
+        <div style= "width:45%; height:400px;">
+            <div style="margin-top:365px; width:100%;">
+                <div class="text-center display-4">April, 2024</div>
+                <div class="line"></div>
+                <div class="text-center" style="margin:32px 0 32px 0;">
+                  <span class="h5" id="webpage-width"></span>
+              </div>
+            </div>
+        </div>
+        </div>`;
+     } if(document.documentElement.clientWidth < 465){
+      flex.innerHTML = `
+        <div class="flex" id="flex">
+          <div style="width:45%; height:400px;">
+            <div style="margin-top:10px; width:100%;">
+                <div class="text-center display-4">April, 2023</div>
+                <div style="border:2px solid red; width:100%;"></div>
+                <div class="text-center" style="margin:32px 0 22px 0;">
+                  <span class="h5">Entered <a href="https://nnn.ed.jp/">N high school</a></span>
+                </div>
+                <p style="width:100%;">N high school, which changed my life in any way such as programming, clubs, N-yobikou and events,  which the school able to provides. It's always keeping me busy. I can say that my life-changing started from here.</p>
+            </div>
+          </div>
+          <div style="position:relative; width:10%; text-align:center; margin:0 auto;">
+            <div class="timeframe-line"></div>
+           <div class="event-point" style="height:35px; margin:50px auto;"></div>
+           <div class="event-point" style="height:35px; margin:400px auto;"></div>
+          </div>
+        <div style= "width:45%; height:400px;">
+            <div style="margin-top:365px; width:100%;">
+                <div class="text-center display-4">April, 2024</div>
+                <div class="line"></div>
+                <div class="text-center" style="margin:32px 0 32px 0;">
+                  <span class="h5" id="webpage-width"></span>
+              </div>
+            </div>
+        </div>
+        </div>`;
+     }
+    new TypeIt("#navbar-br", {
       speed: 50,
       waitUntilVisible: true
     })
@@ -118,45 +123,6 @@ window.addEventListener("DOMContentLoaded", () => {
     .type("ta", {delay: 300})
     .delete(1, {delay: 1000})
     .type("o")
-    .go();
-
-    new TypeIt("#text1", {
-      speed: 50,
-      waitUntilVisible: true
-    })
-    .type("I borrowed bubble example in p5js's official website to do this. I produce the different colored bubbles in limited range of each value in rgb randomly. Especially, red in rgb is from 0 to 1. Gree")
-    .move(-6)
-    .type("73")
-    .move(6)
-    .type("n in rgb is from 0 to 1. Blue")
-    .move(-7)
-    .type("2")
-    .move(1)
-    .type("6")
-    .move(6)
-    .type(" in rgb is from 130 to 256.")
-    .go();
-
-    new TypeIt("#text2", {
-      speed: 50,
-      waitUntilVisible: true
-    })
-    .type("This bot is chat-based English dictionary, which can search, create, update meaning of specific word.")
-    .go();
-
-    new TypeIt("#text3", {
-      speed: 50,
-      waitUntilVisible: true
-    })
-    .type("This bot search video with ")
-    .move(-6)
-    .type("s")
-    .move(6)
-    .type("search query on Edge Video.")
-    .move(-7)
-    .delete(4)
-    .type("Bing")
-    .move(7)
     .go();
   }, 9500);
 });
