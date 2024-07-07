1f7b053b341c44ba0d23dbdf99ff91f57fed375d
hiroto takao <biiiy080121@outlook.com>
2024-07-07 21:21:52 +0900

    create image holder to store images of my portfolio website

--- about/loading.js
+++
@@ -1,4 +0,0 @@
-const loadingPage = document.getElementById("loading-page");
-loadingPage.style.display = "block";
-document.body.style.backgroundColor = "yellow";
-document.getElementById("main-page").style.display = "none";
